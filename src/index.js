49cac07ce79da95683eeb6e7d58d99718bd3e952
marloBarksdale <lyndenflood@gmail.com>
2021-08-26 22:28:45 -0400

    added html-webpack-plugin, added image and css loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log("Hello restaurant");
+import pic from './restaurant1.jpg';
+
+
+const content = document.createElement('div');
+
+function component() {
+
+    
+    const element = document.createElement('div');
+
+    element.textContent = "Hello from Restaurant";
+    const img = new Image();
+
+     img.src = pic;
+
+    element.appendChild(img);
+
+    return element;
+}
+
+document.body.append(content.appendChild(component()));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:"development",
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: "development",
+    entry: {
+       index: './src/index.js'
+    },
+
+    plugins: [
+           new HtmlWebpackPlugin({
+              title: 'Output Management',
+            }),
+          ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean:true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                exclude: path.resolve(__dirname, '/node_modules'),
+            },
+        ],
+    },
 };
